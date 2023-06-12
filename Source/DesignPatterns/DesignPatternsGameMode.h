7224070d0d114aa98f496927eb4605f97d7eb139
Frozenheart32 <alexroid32@gmail.com>
2023-06-12 18:28:57 +0300

    Install project

---
+++ Source/DesignPatterns/DesignPatternsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DesignPatternsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADesignPatternsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADesignPatternsGameMode();
+};
+
+
+
